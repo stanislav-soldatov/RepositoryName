14b1ebb60e1a96cc3ee0cdeb8803ba82bffc88d6
stanislav-soldatov <149319618+stanislav-soldatov@users.noreply.github.com>
2023-11-17 22:59:03 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/1 задание.py
@@ -0,0 +1,17 @@
+# TODO Напишите функцию для поиска индекса товара
+
+def findex(a, b):
+    for index, item in enumerate(b):
+        if item == a:
+            return index
+    return None
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = findex(find_item, items_list)   # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
